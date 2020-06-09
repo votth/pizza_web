f7156ae139fcd006b4419b3d65648a26399472dc
Bazsimo <balala.bazsi@gmail.com>
2020-06-09 22:30:36 +0200

    navbar added for looks placeholder welcome added some default routs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PageNotFoundComponent } from './httpErrors/page-not-found/page-not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PageNotFoundComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: WelcomeComponent, pathMatch: 'full'},
+      { path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
