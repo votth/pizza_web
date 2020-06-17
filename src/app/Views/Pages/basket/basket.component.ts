986ff876d0e2663230fd3bb99064be0b76458ebd
Catnips <voth998@gmai.com>
2020-06-17 16:33:54 +0200

    Basket page overall design

---
+++ src/app/Views/Pages/basket/basket.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  styleUrls: ['./basket.component.css']
+})
+export class BasketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  navigate(page: Event) {
+    console.log(page.target);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Views/Components/nav-bar/nav-bar.component';
 import { PageNotFoundComponent } from './Views/Pages/page-not-found/page-not-found.component';
 import { WelcomeComponent } from './Views/Pages/welcome/welcome.component';
-import { AppRoutingModule } from './app-routing.module';
 import { CreatePizzaComponent } from './Views/Pages/create-pizza/create-pizza.component';
-import {MealListComponent} from './Views/meal-list/meal-list.component';
-import {CurrencyHufPipe} from './Pipes/currency-huf.pipe';
-import {FiveStarComponent} from './Views/five-star/five-star.component';
-import {DrinkListComponent} from './Views/drink-list/drink-list.component';
-import {FormsModule} from '@angular/forms';
-import {AuthGuard} from './Guards/auth.guard';
-import {UserService} from './Services/user.service';
 import { RegisterComponent } from './Views/Pages/register/register.component';
 import { ResetPasswordComponent } from './Views/Pages/reset-password/reset-password.component';
+import { BasketComponent } from './Views/Pages/basket/basket.component';
+
+import { CurrencyHufPipe } from './Pipes/currency-huf.pipe';
+import { MealListComponent } from './Views/meal-list/meal-list.component';
+import { FiveStarComponent } from './Views/five-star/five-star.component';
+import { DrinkListComponent } from './Views/drink-list/drink-list.component';
 import { ListsComponent } from './Views/lists/lists.component';
 import { IngredientByIdPipe } from './Pipes/ingredient-by-id.pipe';
+
+import { AuthGuard } from './Guards/auth.guard';
+import { UserService } from './Services/user.service';
+
 
 @NgModule({
   declarations: [
@@ -36,6 +40,7 @@ import { IngredientByIdPipe } from './Pipes/ingredient-by-id.pipe';
     ResetPasswordComponent,
     ListsComponent,
     IngredientByIdPipe,
+    BasketComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,6 +50,7 @@ import { IngredientByIdPipe } from './Pipes/ingredient-by-id.pipe';
       { path: 'register', component: RegisterComponent},
       { path: 'reset-password', component: ResetPasswordComponent},
       { path: 'menu', component: ListsComponent},
+      { path: 'basket', component: BasketComponent},
       { path: '**', component: PageNotFoundComponent},
     ]),
     AppRoutingModule,
