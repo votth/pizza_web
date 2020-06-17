338300dc2f5f4dd167df92e5a54dc8fc60c4bfeb
Catnips <voth998@gmai.com>
2020-06-17 23:06:30 +0200

    Add placeholder List for basket, need back-end

--- src/app/Views/Pages/basket/basket.component.ts
+++ src/app/Views/Pages/basket/basket.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// Maybe can just import from the basket and build a list from there?
+
+// import {AbstractListComponent} from '../../Components/abstract-list/abstract-list.component';
+// import {Pizza} from '../../../Models/Pizza';
+// import {pizzas} from '../../../HardDatabase/DatabaseHelper';
+// import {Drink} from '../../../Models/Drink';
+// import {drinks} from '../../../HardDatabase/DatabaseHelper';
+
+
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
