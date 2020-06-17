08e8952e3778cde31b12f9e2b48cec3e25b7355b
Catnips <voth998@gmai.com>
2020-06-18 01:05:53 +0200

    Print out basket items

--- src/app/Views/Pages/basket/basket.component.ts
+++ src/app/Views/Pages/basket/basket.component.ts
@@ -4,21 +4,14 @@ import { Pizza } from '../../../Models/Pizza';
 import { Drink } from '../../../Models/Drink';
 import {UserService} from '../../../Services/user.service';
 
-// Maybe can just import from the basket and build a list from there?
-
-// import {AbstractListComponent} from '../../Components/abstract-list/abstract-list.component';
-// import {Pizza} from '../../../Models/Pizza';
-// import {pizzas} from '../../../HardDatabase/DatabaseHelper';
-// import {Drink} from '../../../Models/Drink';
-// import {drinks} from '../../../HardDatabase/DatabaseHelper';
-
-
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent implements OnInit {
+  basketPizzas: any;
+  basketDrinks: any;
 
   constructor(private basketService: ShoppingBasketService, private userService: UserService) { }
 
@@ -26,10 +19,13 @@ export class BasketComponent implements OnInit {
     const loggedInUser = this.userService.getLoggedInUser();
     const basketPizzas: Pizza[] = this.basketService.getBasketPizzas(loggedInUser.shoppingBasket);
     const basketDrinks: Drink[] = this.basketService.getBasketDrinks(loggedInUser.shoppingBasket);
-
+    
     console.log(loggedInUser);
     console.log(basketPizzas);
     console.log(basketDrinks);
+
+    this.basketPizzas = basketPizzas;
+    this.basketDrinks = basketDrinks;
   }
 
   navigate(page: Event) {
